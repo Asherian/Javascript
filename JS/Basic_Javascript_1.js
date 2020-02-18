116cb7478ab8e554b0bcb3b4a151bdb703a9bed4
Asherian <Asherian@outlook.com>
2020-02-17 19:39:31 -0800

    Uploading challenge project
    First Challenges

---
+++ JS/Basic_Javascript_1.js
@@ -0,0 +1,14 @@
+var A="This is a string";
+
+alert("Hi, thank you\'all for coming here.")
+var family="Asherian", son="Zeke", parent="Krist", heir="Morgan";
+var family = family.fontcolor("blues");
+document.write(heir +" "+ family);
+document.write(blues);
+var expression=10+23;
+document.write(expression);
+function My_First_Function(){
+    var str= "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
