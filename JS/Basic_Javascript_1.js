c4abec9071d85c6f0f73ab8b5b8a515ba2d05628
Asherian <Asherian@outlook.com>
2020-02-20 13:41:30 -0800

    Post first debug
    First debug on JS 1 file. Starting JS 2 file.

--- JS/Basic_Javascript_1.js
+++ JS/Basic_Javascript_1.js
@@ -1,13 +1,12 @@
 var A="This is a string";
 
-alert("Hi, thank you\'all for coming here.")
-var family="Asherian", son="Zeke", parent="Krist", heir="Morgan";
-var family = family.fontcolor("blues");
-document.write(heir +" "+ family);
-document.write(blues);
+alert("Hi, thank ya\'ll for coming here.")
+var family= last="Asherian", son="Zeke", parent="Krist", heir="Morgan";
+var family=family.fontcolor("blues");
 var expression=10+23;
-document.write(expression);
-function My_First_Function(){
+document.write(heir +" "+ family + " "+expression);
+
+function My_First_Function() {
     var str= "This text is green!";
     var result = str.fontcolor("green");
     document.getElementById("Green_Text").innerHTML = result;
---
+++ JS/Basic_Javascript_2.js
@@ -0,0 +1,5 @@
+var container = bottle="32oz", bucket="5gal", drum="55gal";
+function changeColor(newColor) {
+    var elem= document.getElementById('Water');
+    elem.style.color = newColor;
+}
