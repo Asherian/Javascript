5bba16d9f758ebd34ba32476fc6630188e82e4bb
Asherian <Asherian@outlook.com>
2020-02-20 14:01:11 -0800

    Concatenate String
    Concatenate challenge.

--- JS/Basic_Javascript_2.js
+++ JS/Basic_Javascript_2.js
@@ -3,3 +3,8 @@ function changeColor(newColor) {
     var elem= document.getElementById('Water');
     elem.style.color = newColor;
 }
+function myFunction() {
+    var sentence = "It's important to find balance to stay healthy.";
+    sentence += " Our bodies are worth putting the effort into";
+    document.getElementById("food").innerhtml = sentence;
+}
